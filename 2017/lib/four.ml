3a3d4f492bc6ac0a21c8b878f046a82fc001d7c4
Patrick Rogers <patrick@helloresolven.com>
2017-12-03 21:31:49 -0800

    day four.

--- 2017/lib/four.ml
+++ 2017/lib/four.ml
@@ -1,2 +1,27 @@
+open Core
+
+let all_unique_words words =
+    let set = String.Set.of_list words in
+    String.Set.length set = List.length words
+
+let sort_chars word =
+    String.to_list word
+    |> List.sort ~cmp:Char.compare
+    |> String.of_char_list
+
+let sort_words words =
+    List.map words ~f:sort_chars
+
+let all_anagrams = Fn.compose all_unique_words sort_words
+
 let solve () =
-    ()
+    let split_words = String.split ~on:' ' in
+    let passphrases = In_channel.read_lines "./2017/data/4.txt" |> List.map ~f:split_words in
+
+    List.filter passphrases ~f:all_unique_words
+    |> List.length
+    |> printf "a: %d\n";
+
+    List.filter passphrases ~f:all_anagrams
+    |> List.length
+    |> printf "b: %d\n";
