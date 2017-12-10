05d5c87110c692be3851d8c4bdab0a73e690b370
Patrick Rogers <patrick@helloresolven.com>
2017-12-09 23:11:34 -0800

    refactor hashing pipeline.

--- 2017/10/main.ml
+++ 2017/10/main.ml
@@ -56,10 +56,10 @@ let read_input () =
   List.append additional_lengths [17; 31; 73; 47; 23]
 
 let _ =
-  let input = read_input () in
-  let sparse_hash = create_sparse_hash input in
-  let dense_hash = create_dense_hash sparse_hash in
-  let to_hex = sprintf "%02x" in
-  let hashed = Array.map dense_hash ~f:to_hex |> Array.to_list |> String.concat in
-  (* assert (String.equal hashed "e1462100a34221a7f0906da15c1c979a"); *)
-  printf "hash: %s\n" hashed
+  let hash = read_input ()
+             |> create_sparse_hash
+             |> create_dense_hash
+             |> Array.map ~f:(sprintf "%02x")
+             |> Array.to_list
+             |> String.concat in
+  printf "hash: %s\n" hash
