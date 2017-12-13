370bb9c6cb40d8ea36254bc92bc7a9f598d59c8d
Patrick Rogers <patrick@helloresolven.com>
2017-12-13 08:20:37 -0800

    Update to remove the brute-forced simulation... can merely check if the security system would be at 0 at a layer's n.

--- 2017/13/main.ml
+++ 2017/13/main.ml
@@ -1,53 +1,35 @@
 open Core
 
 module Layer = struct
-  type direction = Up | Down
-  type t = { n:int; depth:int; current:int; dir:direction; }
+  type t = { n:int; depth:int; }
 
-  let is_zero t = t.current = 0
+  let will_catch layer delay =
+    (layer.n + delay) % (2 * layer.depth - 2) = 0
 
-  let increment_time layer =
-    let move_down layer =
-      let next = layer.current + 1 in
-      if next = layer.depth then {layer with current = (layer.current - 1); dir = Up; }
-      else {layer with current = (layer.current + 1); }
-    and move_up layer =
-      if layer.current = 0 then {layer with current = (layer.current + 1); dir = Down; }
-      else {layer with current = (layer.current - 1); }
-    in
-    match layer.dir with
-    | Down -> move_down layer
-    | Up -> move_up layer
+  let severity layer = layer.n * layer.depth
 end
 
-let next_step map =
-  Int.Map.map map ~f:Layer.increment_time
+let is_caught layers delay =
+  let f layer = Layer.will_catch layer delay in
+  List.find layers ~f 
+  |> Option.is_some
 
-let traverse map =
-  let until, _ = Int.Map.max_elt_exn map in
-  let rec aux map step caught =
-    if step > until || not (List.is_empty caught) then caught
-    else
-      let caught = match Int.Map.find map step with
-        | Some layer -> if Layer.is_zero layer then layer::caught else caught
-        | None -> caught
-      in
-      let new_map = next_step map in
-      aux new_map (step+1) caught
-  in aux map 0 []
+let severity_of_traversal layers delay =
+  let f layer = Layer.will_catch layer delay in
+  List.filter layers ~f
+  |> List.map ~f:Layer.severity
+  |> List.fold ~init:0 ~f:Int.(+)
 
-let find_safe_time map =
-  let rec aux map n =
-    let map = next_step map in
-    match traverse map with
-    | [] -> n
-    | _ -> aux map (n+1)
-  in aux map 1
+let find_safe_time layers =
+  let rec aux layers n =
+    if is_caught layers n then aux layers (n + 1)
+    else n
+  in aux layers 0
 
 let parse_inputs () =
   let parse_line line =
     let to_layer = function
-      | [n; depth] -> Some Layer.{n; depth; current=0; dir=Down}
+      | [n; depth] -> Some Layer.{n; depth;}
       | _ -> None
     in
     String.split line ~on:':'
@@ -57,9 +39,10 @@ let parse_inputs () =
   let open Layer in
   In_channel.read_lines "./input.txt"
   |> List.filter_map ~f:parse_line
-  |> List.fold ~init:(Int.Map.empty) ~f:(fun acc layer -> Map.add acc ~key:layer.n ~data:layer)
 
 let _ =
   let input = parse_inputs () in
+  let severity = severity_of_traversal input 0 in
+  printf "a: %d\n" severity;
   let brute = find_safe_time input in
   printf "b: %d\n" brute;
