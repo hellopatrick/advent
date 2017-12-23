9641b301e2794390732d22aab9df820f81f2fe82
Patrick Rogers <patrick@helloresolven.com>
2017-12-22 23:18:04 -0800

    better expl.

--- 2017/23/main.ml
+++ 2017/23/main.ml
@@ -1,37 +1,37 @@
 open Core
 
-(* 
+    (*
 
-this is the assembly translated to C-like.
-it is looking for non-primes between c & b. 
+    this is the assembly translated to C-like.
+    it is looking for non-primes in { k | i >= 0 AND b + 17 * i < c }
 
-b = 65*100 + 100_000
-c = b + 17_000
+    b = 65*100 + 100_000
+    c = b + 17_000
 
-for b=65; b < c; b += 17 {
-    f = 1
-    d = 2
-    e = 2
+    for b=65; b < c; b += 17 {
+        f = 1
+        d = 2
+        e = 2
 
-    for d=2; d < b; d++ {
-        for e=2; e < b; e++ {
-            if b = d * e {
-                f = 0
+        for d=2; d < b; d++ {
+            for e=2; e < b; e++ {
+                if b = d * e {
+                    f = 0
+                }
             }
+        }
+
+        if(f==0) {
+            h+=1
         }
     }
 
-    if(f==0) {
-        h+=1
-    }
-}
+    optimizations:
+    - break loop early.
+    - only really need to check b % d = 0
+    - only check d from 2 to sqrt(b)
 
-optimizations:
-- break loop early.
-- only really need to check b % d = 0
-- only check d from 2 to sqrt(b)
-
-*)
+    *)
 
 let b = 65 * 100 + 100_000
 let c = b + 17_000
