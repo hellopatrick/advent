a0a0ddb9e2559354b0d0d9d051c1a7f756016b8a
Patrick Rogers <patrick@helloresolven.com>
2017-12-25 00:35:41 -0500

    apply_n_times is our loop.

--- 2017/25/main.ml
+++ 2017/25/main.ml
@@ -1,12 +1,8 @@
 open Core
 
-let rec loop state n =
-  if n = 0 then state
-  else loop (Machine.transition state) (n-1)
-
 let _ =
   let state = Machine.{tape=Int.Map.empty; current_state=A; cursor=0} in
-  let final = loop state 12_667_664 in
+  let final = Fn.apply_n_times ~n:12_667_664 Machine.transition state in
   Int.Map.data final.tape
   |> List.count ~f:(fun x -> x = 1)
   |> printf "%d\n"
