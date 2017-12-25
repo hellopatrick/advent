f127e5dbd0807fc8c200f2601f201ac62fafdce6
Patrick Rogers <patrick@helloresolven.com>
2017-12-25 16:39:22 -0500

    use set instead of map.

--- 2017/25/machine.ml
+++ 2017/25/machine.ml
@@ -2,22 +2,22 @@ open Core
 
 type state = A | B | C | D | E | F
 
-type t = { tape: int Int.Map.t; current_state:state; cursor:int; }
+type t = { tape:Int.Set.t; current_state:state; cursor:int; }
 
 let current_value t =
-  Int.Map.find t.tape t.cursor
-  |> Option.value ~default:0
+  Int.Set.mem t.tape t.cursor
 
 let transition t =
-  let aux state value =
-    match state with
-    | A -> if value = 0 then (1, 1, B) else (0, -1, C)
-    | B -> if value = 0 then (1, -1, A) else (1, 1, D)
-    | C -> if value = 0 then (0, -1, B) else (0, -1, E)
-    | D -> if value = 0 then (1, 1, A) else (0, 1, B)
-    | E -> if value = 0 then (1, -1, F) else (1, -1, C)
-    | F -> if value = 0 then (1, 1, D) else (1, 1, A)
+  let aux t =
+    let on = Int.Set.mem t.tape t.cursor in
+    match t.current_state with
+    | A -> if not on then (Int.Set.add, 1, B) else (Int.Set.remove, -1, C)
+    | B -> if not on then (Int.Set.add, -1, A) else (Int.Set.add, 1, D)
+    | C -> if not on then (Int.Set.remove, -1, B) else (Int.Set.remove, -1, E)
+    | D -> if not on then (Int.Set.add, 1, A) else (Int.Set.remove, 1, B)
+    | E -> if not on then (Int.Set.add, -1, F) else (Int.Set.add, -1, C)
+    | F -> if not on then (Int.Set.add, 1, D) else (Int.Set.add, 1, A)
   in
-  let data, shift, next = aux t.current_state (current_value t) in
-  let tape = Int.Map.add t.tape ~key:t.cursor ~data in
+  let update, shift, next = aux t in
+  let tape = update t.tape t.cursor in
   { tape; current_state=next; cursor=(t.cursor + shift); }
--- 2017/25/main.ml
+++ 2017/25/main.ml
@@ -1,8 +1,7 @@
 open Core
 
 let _ =
-  let state = Machine.{tape=Int.Map.empty; current_state=A; cursor=0} in
+  let state = Machine.{tape=Int.Set.empty; current_state=A; cursor=0} in
   let final = Fn.apply_n_times ~n:12_667_664 Machine.transition state in
-  Int.Map.data final.tape
-  |> List.count ~f:(fun x -> x = 1)
-  |> printf "%d\n"
+  Int.Set.length final.tape
+  |> printf "%d\n";
