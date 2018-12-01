ee6e525bb626694b4ba8df9535f471de83702e47
Patrick Rogers <patrick@donotforget.xyz>
2018-11-30 22:53:51 -0800

    style: some style changes.

--- 2018/01/solution.ml
+++ 2018/01/solution.ml
@@ -1,35 +1,20 @@
 open Core
 
-let input = In_channel.read_lines "input.txt"
-            |> List.map ~f:Int.of_string
+let input = In_channel.read_lines "input.txt" |> List.map ~f:Int.of_string
+let part_one = input |> List.reduce ~f:( + ) |> Option.value ~default:0
 
-let part_one = 
-  input
-  |> List.reduce ~f:(+)
-  |> Option.value ~default:0
-
-type t = { seen:Int.Set.t; current:Int.t }
-
-let running_sum acc curr = 
+let running_sum acc curr =
   let open Container.Continue_or_stop in
-  let next = acc.current + curr in 
-  if (Int.Set.mem acc.seen next) then
-    Stop next
-  else 
-    let seen = Int.Set.add acc.seen next in 
-    let current = next in 
-    Continue {seen; current}
+  let seen, value = acc in
+  let next = value + curr in
+  if Set.mem seen next then Stop next else Continue (Set.add seen next, next)
 
-let finish t =
-  t.current
+let finish _ = -1
 
-let part_two = 
-  let seen = Int.Set.empty in 
-  let current = 0 in 
-  input
-  |> Sequence.cycle_list_exn
-  |> Sequence.fold_until ~init:{seen; current} ~f:running_sum ~finish
+let part_two =
+  let init = (Int.Set.empty, 0) in
+  input |> Sequence.cycle_list_exn
+  |> Sequence.fold_until ~init ~f:running_sum ~finish
 
-let _ = 
-  printf "%d\n" part_one;
-  printf "%d" part_two;
+let _ =
+  printf "%d" part_one ; Out_channel.newline stdout ; printf "%d" part_two
