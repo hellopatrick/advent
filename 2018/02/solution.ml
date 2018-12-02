98c79286ed104380c8d9675ba6a28b83c12d2739
Patrick Rogers <patrick@donotforget.xyz>
2018-12-02 00:20:14 -0800

    refactor: small refactors

--- 2018/02/solution.ml
+++ 2018/02/solution.ml
@@ -4,19 +4,18 @@ let input () = In_channel.read_lines "./input.txt"
                |> List.map ~f:String.to_list
 
 let part_one input = 
-  let count chars =
-    let f map key =
-      let update = function
-        | None -> 1
-        | Some v -> v+1 in 
-      Map.update map key ~f:update
-    in List.fold ~init:Char.Map.empty ~f chars 
+  let counts chars =
+    let updater = (Option.value_map ~default:1 ~f:(fun v -> v + 1)) in 
+    let f = Map.update ~f:updater in 
+    List.fold ~init:Char.Map.empty ~f chars 
   in 
-  let char_counts = List.map ~f:count input in 
-  let has count = Char.Map.exists ~f:(fun v -> v = count) in
+  let char_counts = List.map ~f:counts input in 
+  let has count = Char.Map.exists ~f:(Int.equal count) in
   let twos = List.count ~f:(has 2) char_counts in 
   let threes = List.count ~f:(has 3) char_counts in 
   twos * threes
+
+exception Impossible
 
 let part_two input = 
   let f (a, b) = 
@@ -30,7 +29,7 @@ let part_two input =
   | Some (a, b) -> List.zip_exn a b 
                    |> List.filter_map ~f:(fun (a, b) -> if a = b then Some a else None) 
                    |> String.of_char_list
-  | None -> ""
+  | None -> raise Impossible
 
 let _ = 
   printf "%d" (part_one (input ()));
